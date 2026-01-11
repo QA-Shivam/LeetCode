39ebbd2d6445eb63689dba87f87513d1eec56fc5
QA-Shivam <shivamkumar16399@gmail.com>
2026-01-11 20:19:32 +0530

    solution

---
+++ 30 Days of JavaScript/Return Length of Arguments Passed/solution.js
@@ -0,0 +1,17 @@
+/**
+ * @param {...(null|boolean|number|string|Array|Object)} args
+ * @return {number}
+ */
+var argumentsLength = function(...args) {
+    // return args.length;
+    let count=0;
+    for(let i in args){
+        count++;
+    }
+    return count;
+    
+};
+
+/**
+ * argumentsLength(1, 2, 3); // 3
+ */
